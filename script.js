9e785528d8f371bed39223e0fd31e0fb694c106e
hassannh <hassannouhi91@gmail.com>
2022-10-28 10:33:42 +0200

    style

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ var get = document.getElementById("whilehovering")
 
 function toggleMenu() {
 get.classList.toggle("hide-menu")
-
 }
+
+
+// var dishes = document.getElementsByClass("wrapper")
+// var o_table = document.getElementsByClassName("order")
+
+// for (var i = 0; i < dishes.length; i++) {
+// var event = addEventListener("click.value")
+// }
