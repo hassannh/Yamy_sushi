9fd0d0e0c682a2e09ac901d90f4f85bc98ffbd59
hassannh <hassannouhi91@gmail.com>
2022-10-28 20:21:43 +0200

    js

--- script.js
+++ script.js
@@ -4,10 +4,68 @@ function toggleMenu() {
 get.classList.toggle("hide-menu")
 }
 
+var total = 0;
 
-// var dishes = document.getElementsByClass("wrapper")
-// var o_table = document.getElementsByClassName("order")
+ var dishes = document.querySelectorAll(".wrapper")
+ var SALADE = document.getElementsByClassName("caaa")
+ var SUSHI = document.querySelectorAll(".caa")
+ var OBENTO = document.querySelectorAll(".c")
+ function OnGradeChanged(value){
+    switch (value) {
+        case "SALADE":
+            for (let index = 0; index < SUSHI.length; index++) {
+                SUSHI[index].classList.add("disabled_list");
+            }
+            for (let index = 0; index < OBENTO.length; index++) {
+                OBENTO[index].classList.add("disabled_list");
+            }
+            for (let index = 0; index < SALADE.length; index++) {
+                SALADE[index].classList.remove("disabled_list");
+            }
+            break;
+        case "SUSHI":
+            for (let index = 0; index < SALADE.length; index++) {
+                SALADE[index].classList.add("disabled_list");
+            }
+            for (let index = 0; index < OBENTO.length; index++) {
+                OBENTO[index].classList.add("disabled_list");
+            }
+            for (let index = 0; index < SUSHI.length; index++) {
+                SUSHI[index].classList.remove("disabled_list");
+            }
+            break;
+        case "OBENTO":
+            for (let index = 0; index < SUSHI.length; index++) {
+                SUSHI[index].classList.add("disabled_list");
+            }
+            for (let index = 0; index < SALADE.length; index++) {
+                SALADE[index].classList.add("disabled_list");
+            }
+            for (let index = 0; index < OBENTO.length; index++) {
+                OBENTO[index].classList.remove("disabled_list");
+            }
+            break;
+        default:
+            for (let index = 0; index < SUSHI.length; index++) {
+                SUSHI[index].classList.remove("disabled_list");
+            }
+            for (let index = 0; index < SALADE.length; index++) {
+                SALADE[index].classList.remove("disabled_list");
+            }
+            for (let index = 0; index < OBENTO.length; index++) {
+                OBENTO[index].classList.remove("disabled_list");
+            }
+            break;
+    }
 
-// for (var i = 0; i < dishes.length; i++) {
-// var event = addEventListener("click.value")
-// }
+}
+
+function moveToCarte(el){
+    var cl = el.cloneNode(true);
+    total += parseFloat(el.children[3].textContent);
+    console.log(parseFloat(el.children[3].textContent));
+    console.log(total);
+    document.getElementById("cart-content").appendChild(cl);
+    document.getElementById("total-price").textContent = total;
+}
+
